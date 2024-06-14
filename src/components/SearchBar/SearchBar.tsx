29e5b23459a13d7b6ca28e93c22cdad6449d8062
hoan <hoandvx@gmail.com>
2024-06-14 16:36:30 +0700

    search

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,18 +1,38 @@
 // import React from 'react';
 import SearchIcon from '@mui/icons-material/Search';
-import './SearchBar.css'
+import './SearchBar.css';
+import { useState } from 'react';
 
 function SearchBar() {
-    return (
-        <div className='search'>
-            <div className='searchInput'>
-                <input type='text' placeholder='Search' />
-                <div className='searchIcon'> 
-                    <SearchIcon/>
-                </div>   
-            </div>
+  const [search, setSearch] = useState();
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  const changeSearch = (event: any) => {
+    setSearch(event.target.value);
+    console.log(event.target.value);
+  };
+
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  //   const searchKeyDown = (event: any) => {
+  //     if (event.key === 'Enter') {
+  //       props.changeHandleSearch(search);
+  //     }
+  //   };
+  return (
+    <div className='search'>
+      <div className='searchInput'>
+        <input
+          type='text'
+          placeholder='Search'
+          onChange={changeSearch}
+          //   onKeyDown={searchKeyDown}
+          value={search}
+        />
+        <div className='searchIcon'>
+          <SearchIcon />
         </div>
-    );
+      </div>
+    </div>
+  );
 }
 
 export default SearchBar;
